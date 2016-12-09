9ab53115b0ec38ceb9827846e16923d2ac47662e
sebastian <sebmuell@phys.ethz.ch>
2016-12-09 10:16:54 +0100

    towards a python package

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='acp_effective_area',
+    version='0.0.0',
+    description='Simulate the instrument response of an Atmospheric Cherenkov Plenoscope (ACP)',
+    url='https://github.com/thebiglebowsky/effective_area.git',
+    author='Sebastian Achim Mueller',
+    author_email='sebmuell@phys.ethz.ch',
+    license='MIT',
+    packages=[
+        'acp_effective_area',
+    ],
+    package_data={'acp_effective_area': ['resources/*']},
+    install_requires=[
+        'docopt',
+    ],
+    entry_points={'console_scripts': [
+        'acp_effective_area = acp_effective_area.main:main',
+    ]},
+    zip_safe=False,
+)
