6e863bc87f3b2947b21dd9afa1cd176eef5a78a7
sebastian <sebmuell@phys.ethz.ch>
2017-03-17 21:30:08 +0100

    add more features and spikes to the CTA nsb flux curve.

--- acp_effective_area/resources/night_sky_background_differential_flux.py
+++ acp_effective_area/resources/night_sky_background_differential_flux.py
@@ -3,10 +3,10 @@ La Palma night sky background light
 Taken from:
 
 @article{gaug2013night,
-  title={Night Sky Background Analysis for the Cherenkov Telescope Array using the Atmoscope instrument},
-  author={Gaug, Markus and others},
-  journal={arXiv preprint arXiv:1307.3053},
-  year={2013}
+    title={Night Sky Background Analysis for the Cherenkov Telescope Array using the Atmoscope instrument},
+    author={Gaug, Markus and others},
+    journal={arXiv preprint arXiv:1307.3053},
+    year={2013}
 }
 
 Figure 4. The CTA icrc2013 proceeding claims the data (black solid line) of 
@@ -24,21 +24,41 @@ c = 299792458
 # 1 Jy = 10^-26 J/(s m^2 Hz)
 # wavelength [nm], flux [Jy / (deg^2)] // [10^-26 J/(s m^2 Hz deg^2)]
 raw = np.array([
-	[240, 8], # extrapolated
-	[270, 11], # extrapolated
-	[300, 14.7], # extrapolated
-	[330, 15.7],
-	[340, 20.0],
-	[395, 30.0],
-	[432, 40.0],
-	[462, 50.0],
-	[485, 60.0],
-	[525, 80.0],
-	[560, 100.0],
-	[600, 130.0],
-	[650, 140.0],
-	[701, 140.0],
-	])
+    [240, 8], # extrapolated
+    [270, 11], # extrapolated
+    [300, 14.7], # extrapolated
+    [330, 15.7],
+    [340, 20],
+    [394, 30],
+    [395, 145],
+    [396, 30],
+    [433, 40],
+    [435, 81],
+    [437, 40],
+    [462, 50],
+    [485, 60],
+    [525, 80],
+    [555, 97],
+    [557.5, 948],
+    [561, 93],
+    [585, 105],
+    [591, 424],
+    [595, 146],
+    [600, 130],
+    [605, 106],
+    [627, 161],
+    [630, 851],
+    [632, 145],
+    [638, 371],
+    [640, 105],
+    [645, 105],
+    [647, 140],
+    [660, 140],
+    [670, 105],
+    [678, 105],
+    [685, 217],
+    [701, 140],
+    ])
 
 raw[:,0] *= 1e-9 # from nm to m -> wavelength [m]
 
@@ -60,14 +80,14 @@ raw[:,1] /= ((h*c)/raw[:,0]) # flux [1 / (s m^2 m sr)]
 """
 Taken from:
 @article{preuss2002study,
-  title={Study of the photon flux from the night sky at La Palma and Namibia, in the wavelength region relevant for imaging atmospheric Cherenkov telescopes},
-  author={Preuss, S and Hermann, G and Hofmann, W and Kohnle, A},
-  journal={Nuclear Instruments and Methods in Physics Research Section A: Accelerators, Spectrometers, Detectors and Associated Equipment},
-  volume={481},
-  number={1},
-  pages={229--240},
-  year={2002},
-  publisher={Elsevier}
+    title={Study of the photon flux from the night sky at La Palma and Namibia, in the wavelength region relevant for imaging atmospheric Cherenkov telescopes},
+    author={Preuss, S and Hermann, G and Hofmann, W and Kohnle, A},
+    journal={Nuclear Instruments and Methods in Physics Research Section A: Accelerators, Spectrometers, Detectors and Associated Equipment},
+    volume={481},
+    number={1},
+    pages={229--240},
+    year={2002},
+    publisher={Elsevier}
 }
 Figure 8
 """
@@ -81,12 +101,24 @@ hoffmann = np.array([
     [550.0e-9, 7.3e18],
 ])
 
-
-
 plt.plot(raw[:,0], raw[:,1], label='benn')
 plt.plot(hoffmann[:,0], hoffmann[:,1], 'r', label='hoff')
-
+plt.semilogy()
 plt.legend(('Benn and Ellision (inter and extrapolated)', 'W. Hoffmann and friends'), loc='lower right')
 plt.xlabel('wavelength/m')
 plt.ylabel('nsb flux [1/(s m^2 sr m)]')
-plt.show()
+plt.savefig('nsb_CTA_and_Benn_and_Ellison_and_Hoffmann.png')
+
+xml = ''
+xml+= '<function name="nsb_flux_vs_wavelength" comment="Night Sky Background Analysis for the Cherenkov Telescope Array using the Atmoscope instrument, Markus Gaug, arXiv preprint arXiv:1307.3053, based on Benn Ch.R., Ellison S.R., La Palma tech. note, 2007">\n'
+xml+= '    <linear_interpolation>\n'
+
+for lambda_flux in raw:
+    xml+= '        <xy x="{wavelength:.2e}" y="{flux:.3e}"/>\n'.format(
+        wavelength=lambda_flux[0],
+        flux=lambda_flux[1])
+
+xml+= '    </linear_interpolation>\n'
+xml+= '</linear_interpolation>\n'
+
+print(xml)
