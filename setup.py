a7326fd04c1a144069fd432f909e5b1a866eb4cf
sebastian <sebmuell@phys.ethz.ch>
2017-04-25 19:07:40 +0200

    Include the full resources tree and not just the top level directory

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
 from distutils.core import setup
+import os
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+setup_py_path = os.path.realpath(__file__)
+setup_py_dir = os.path.dirname(setup_py_path)
+extra_files = package_files(os.path.join(setup_py_dir,'acp_instrument_response_function','resources'))
 
 setup(
     name='acp_instrument_response_function',
@@ -11,7 +23,7 @@ setup(
     packages=[
         'acp_instrument_response_function',
     ],
-    package_data={'acp_instrument_response_function': ['resources/*']},
+    package_data={'acp_instrument_response_function': extra_files},
     install_requires=[
         'docopt',
         'paramiko',
