14efdd7866038de9333b0573b7ffcf872b10a4ec
sebastian <sebmuell@phys.ethz.ch>
2017-04-27 20:35:11 +0200

    remove all the plenoscope resources. These go now to the starter_kit

--- setup.py
+++ setup.py
@@ -1,16 +1,4 @@
 from distutils.core import setup
-import os
-
-def package_files(directory):
-    paths = []
-    for (path, directories, filenames) in os.walk(directory):
-        for filename in filenames:
-            paths.append(os.path.join('..', path, filename))
-    return paths
-
-setup_py_path = os.path.realpath(__file__)
-setup_py_dir = os.path.dirname(setup_py_path)
-extra_files = package_files(os.path.join(setup_py_dir,'acp_instrument_response_function','resources'))
 
 setup(
     name='acp_instrument_response_function',
@@ -23,10 +11,9 @@ setup(
     packages=[
         'acp_instrument_response_function',
     ],
-    package_data={'acp_instrument_response_function': extra_files},
     install_requires=[
         'docopt',
-        'paramiko',
+        'scoop',
     ],
     entry_points={'console_scripts': [
         'acp_instrument_response_function = acp_instrument_response_function.main:main',
