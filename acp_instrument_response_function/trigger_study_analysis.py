c9a4d44faa65d4ed2d84c6a6bb7f6c0aea1260c8
sebastian <sebmuell@phys.ethz.ch>
2018-10-17 18:09:56 +0200

    output the scatter-angles and scatter-radii

--- acp_instrument_response_function/trigger_study_analysis.py
+++ acp_instrument_response_function/trigger_study_analysis.py
@@ -23,12 +23,20 @@ def run_analysis(path, patch_threshold=67):
     thresholds = []
     energies = []
     max_scatter_radii = []
+    scatter_radii = []
+    zenith_distances = []
     for e in events:
         exposure_times.append(
             e['refocus_sum_trigger'][0]['exposure_time_in_slices'])
         energies.append(e['simulation_truth']['energy'])
         num_air_shower_photons.append(e['num_air_shower_pulses'])
         max_scatter_radii.append(e['simulation_truth']['scatter_radius'])
+        scatter_radii.append(
+            np.hypot(
+                e['simulation_truth']['core_position']['x'],
+                e['simulation_truth']['core_position']['y']))
+        zenith_distances.append(
+            e['simulation_truth']['zenith'])
         t0 = e['refocus_sum_trigger'][0]['patch_threshold']
         t1 = e['refocus_sum_trigger'][1]['patch_threshold']
         t2 = e['refocus_sum_trigger'][2]['patch_threshold']
@@ -38,6 +46,8 @@ def run_analysis(path, patch_threshold=67):
     exposure_times = np.array(exposure_times)
     energies = np.array(energies)
     max_scatter_radii = np.array(max_scatter_radii)
+    scatter_radii = np.array(scatter_radii)
+    zenith_distances = np.array(zenith_distances)
 
     # Trigger
     # -------
@@ -218,3 +228,166 @@ def run_analysis(path, patch_threshold=67):
 
     with open(os.path.join(path, 'results', 'irf.csv'), 'wt') as fout:
         fout.write(out)
+
+
+    def add_hisogram_semilogy(
+        ax,
+        bin_counts,
+        bin_counts_error_low,
+        bin_counts_error_high,
+        bin_edges,
+        color='k',
+        alpha_boxes=0.1):
+        for i, y in enumerate(bin_counts):
+            ax.plot(
+                [bin_edges[i], bin_edges[i + 1]],
+                [bin_counts[i], bin_counts[i]],
+                color)
+
+        for i, y in enumerate(bin_counts):
+            ax.fill_between(
+                x=[bin_edges[i], bin_edges[i + 1]],
+                y1=[bin_counts_error_low[i], bin_counts_error_low[i]],
+                y2=[bin_counts_error_high[i], bin_counts_error_high[i]],
+                color=color,
+                alpha=alpha_boxes,
+                linewidth=0)
+
+    figsize = (8, 4)
+    dpi = 240
+    ax_size = (0.13, 0.13, 0.84, 0.84)
+
+    # Visualize impact-scatter-range
+    scatter_radius_squared_bin_edges = np.linspace(
+        0,
+        np.max(scatter_radii[trigger_mask])**2,
+        np.int(np.sqrt(np.sum(trigger_mask))/2))
+
+    scatter_radius_squared_bin_counts_triggered = np.histogram(
+        scatter_radii[trigger_mask]**2,
+        scatter_radius_squared_bin_edges)[0]
+
+    scatter_radius_squared_bin_counts_thrown = np.histogram(
+        scatter_radii**2,
+        scatter_radius_squared_bin_edges)[0]
+
+    with open(os.path.join(path, 'results', 'scatter_radius.json'), 'wt') as fout:
+        fout.write(json.dumps({
+            'scatter_radius_squared_bin_edges':
+                scatter_radius_squared_bin_edges.tolist(),
+            'scatter_radius_squared_bin_counts_triggered':
+                scatter_radius_squared_bin_counts_triggered.tolist(),
+            'scatter_radius_squared_bin_counts_thrown':
+                scatter_radius_squared_bin_counts_thrown.tolist()
+            }))
+
+    count_error = (np.sqrt(scatter_radius_squared_bin_counts_triggered)/
+        scatter_radius_squared_bin_counts_triggered)
+    count_ratio = (
+            scatter_radius_squared_bin_counts_triggered/
+            scatter_radius_squared_bin_counts_thrown)
+
+    fig = plt.figure(figsize=figsize, dpi=dpi)
+    ax = fig.add_axes(ax_size)
+    add_hisogram_semilogy(
+        ax=ax,
+        bin_counts=1e2*count_ratio,
+        bin_counts_error_low=1e2*count_ratio*(1 - count_error),
+        bin_counts_error_high=1e2*count_ratio*(1 + count_error),
+        bin_edges=1e-6*scatter_radius_squared_bin_edges,)
+    ax.set_xlabel(r'(Scatter-radius)$^2$/(km)$^2$')
+    ax.set_ylabel(r'Trigger-probability/%')
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.grid(color='k', linestyle='-', linewidth=0.66, alpha=0.1)
+    fig.savefig(os.path.join(path, 'results', 'scatter_radius.png'))
+
+
+    fig = plt.figure(figsize=figsize, dpi=dpi)
+    ax = fig.add_axes(ax_size)
+    for i in range(scatter_radius_squared_bin_counts_triggered.shape[0]):
+        ax.plot(
+            1e-6*np.array([scatter_radius_squared_bin_edges[i], scatter_radius_squared_bin_edges[i + 1]]),
+            [scatter_radius_squared_bin_counts_triggered[i], scatter_radius_squared_bin_counts_triggered[i]],
+            'k--')
+        ax.plot(
+            1e-6*np.array([scatter_radius_squared_bin_edges[i], scatter_radius_squared_bin_edges[i + 1]]),
+            [scatter_radius_squared_bin_counts_thrown[i], scatter_radius_squared_bin_counts_thrown[i]],
+            'k')
+    ax.semilogy()
+    ax.set_xlabel(r'(Scatter-radius)$^2$/(km)$^2$')
+    ax.set_ylabel(r'Number events/1')
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.grid(color='k', linestyle='-', linewidth=0.66, alpha=0.1)
+    fig.savefig(os.path.join(path, 'results', 'scatter_radius_thrown_and_triggered.png'))
+
+
+    if np.max(zenith_distances[trigger_mask])**2 > 0.0:
+        scatter_angle_squared_bin_edges = np.linspace(
+            0,
+            np.max(zenith_distances[trigger_mask])**2,
+            np.int(np.sqrt(np.sum(trigger_mask))/2))
+
+        scatter_angle_squared_bin_counts_triggered = np.histogram(
+            zenith_distances[trigger_mask]**2,
+            scatter_angle_squared_bin_edges)[0]
+
+        scatter_angle_squared_bin_counts_thrown = np.histogram(
+            zenith_distances**2,
+            scatter_angle_squared_bin_edges)[0]
+
+        with open(os.path.join(path, 'results', 'scatter_angle.json'), 'wt') as fout:
+            fout.write(json.dumps({
+                'scatter_angle_squared_bin_edges':
+                    scatter_angle_squared_bin_edges.tolist(),
+                'scatter_angle_squared_bin_counts_triggered':
+                    scatter_angle_squared_bin_counts_triggered.tolist(),
+                'scatter_angle_squared_bin_counts_thrown':
+                    scatter_angle_squared_bin_counts_thrown.tolist(),
+                }))
+
+        count_error = (np.sqrt(scatter_angle_squared_bin_counts_triggered)/
+            scatter_angle_squared_bin_counts_triggered)
+        count_ratio = (
+                scatter_angle_squared_bin_counts_triggered/
+                scatter_angle_squared_bin_counts_thrown)
+
+        fig = plt.figure(figsize=figsize, dpi=dpi)
+        ax = fig.add_axes(ax_size)
+        add_hisogram_semilogy(
+            ax=ax,
+            bin_counts=1e2*count_ratio,
+            bin_counts_error_low=1e2*count_ratio*(1 - count_error),
+            bin_counts_error_high=1e2*count_ratio*(1 + count_error),
+            bin_edges=scatter_angle_squared_bin_edges,)
+        ax.set_xlabel(r'(Scatter-angle)$^2$/(rad)$^2$')
+        ax.set_ylabel(r'Trigger-probability/%')
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax.grid(color='k', linestyle='-', linewidth=0.66, alpha=0.1)
+        ax.axvline(x=np.deg2rad(3.25)**2, color='k', linestyle=':')
+        fig.savefig(os.path.join(path, 'results', 'scatter_angle.png'))
+
+
+        fig = plt.figure(figsize=figsize, dpi=dpi)
+        ax = fig.add_axes(ax_size)
+        for i in range(scatter_angle_squared_bin_counts_triggered.shape[0]):
+            ax.plot(
+                [scatter_angle_squared_bin_edges[i], scatter_angle_squared_bin_edges[i + 1]],
+                [scatter_angle_squared_bin_counts_triggered[i], scatter_angle_squared_bin_counts_triggered[i]],
+                'k--')
+            ax.plot(
+                [scatter_angle_squared_bin_edges[i], scatter_angle_squared_bin_edges[i + 1]],
+                [scatter_angle_squared_bin_counts_thrown[i], scatter_angle_squared_bin_counts_thrown[i]],
+                'k')
+        ax.semilogy()
+        ax.set_xlabel(r'(Scatter-angle)$^2$/(rad)$^2$')
+        ax.set_ylabel(r'Number events/1')
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax.grid(color='k', linestyle='-', linewidth=0.66, alpha=0.1)
+        ax.axvline(x=np.deg2rad(3.25)**2, color='k', linestyle=':')
+        fig.savefig(os.path.join(path, 'results', 'scatter_angle_thrown_and_triggered.png'))
+
+    plt.close('all')
