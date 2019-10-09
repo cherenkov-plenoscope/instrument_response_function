7d832158917847280455a2847ce42f9babcf3d87
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2019-10-09 15:25:36 +0200

    long readme, line break for pep8

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 import setuptools
 import os
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setuptools.setup(
     name='acp_instrument_response_function',
     version='0.0.3',
-    description='Simulates the instrument response of the ' +
-    'Atmospheric-Cherenkov-Plenoscope (ACP)',
-    url='https://github.com/cherenkov-plenoscope/instrument_response_function.git',
+    description='Simulating the instrument-response of the Portal ' +
+    'Cherenkov-plenoscope',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/cherenkov-plenoscope/' +
+    'instrument_response_function.git',
     author='Sebastian Achim Mueller',
     author_email='sebastian-achim.mueller@mpi-hd.mpg.de',
     license='GPL v3',
